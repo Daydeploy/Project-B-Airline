b4d1620c11694f832bb9d3f1d0e73c241ffd2ca8
AatjeD <aaronbless@hotmail.com>
2024-10-21 13:54:35 +0200

    Sil errors fix ~ Airports

--- Project/DataAccess/AirportAccess.cs
+++ Project/DataAccess/AirportAccess.cs
@@ -3,17 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
-public class AirportModel
-{
-    public int AirportID { get; set; }
-    public string Country { get; set; }
-    public string City { get; set; }
-    public string Name { get; set; }
-    public string Type { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Address { get; set; }
-}
-
 public static class AirportAccess
 {
     private static string path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"DataSources/airports.json"));
---
+++ Project/Logic/AccountService.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class AirportServiceLogic
+{
+    private List<AirportModel> _airports;
+
+    public AirportServiceLogic(List<AirportModel> airports)
+    {
+        _airports = airports;
+    }
+
+    public List<AirportModel> GetLuxuriousAirports()
+    {
+        return _airports.Where(a => a.Type == "Public" && IsAirportLuxurious(a)).ToList();
+    }
+
+    public List<AirportModel> GetLuxuriousPrivateAirstrips()
+    {
+        return _airports.Where(a => a.Type == "Private" && IsAirportLuxurious(a)).ToList();
+    }
+
+    public bool IsAirportLuxurious(AirportModel airport)
+    {
+        // Implement logic to determine if an airport is luxurious
+        // This could be based on various factors like available services, ratings, etc.
+        // For now, we'll use a simple check based on the airport name
+        return airport.Name.Contains("Luxury") || airport.Name.Contains("VIP") || airport.Name.Contains("Executive");
+    }
+
+    public string GetAirportTransportationOptions(AirportModel airport)
+    {
+        // Implement logic to return transportation options
+        return "Taxi, Shuttle, Limousine Service";
+    }
+
+    public string GetNearbyHotels(AirportModel airport)
+    {
+        // Implement logic to return nearby hotels
+        return "Luxury Hotel A, Executive Suite B, VIP Resort C";
+    }
+
+    public string GetAdditionalServices(AirportModel airport)
+    {
+        // Implement logic to return additional services
+        return "Helicopter Transfer, VIP Lounge, Concierge Service";
+    }
+
+    public string GetAirportDescription(AirportModel airport)
+    {
+        // Implement logic to return an interesting description of the airport
+        return $"Experience luxury at its finest at {airport.Name}. " +
+               $"Our world-class facilities and exclusive services ensure an unforgettable journey. " +
+               $"From VIP lounges to personalized concierge, we redefine air travel.";
+    }
+}
