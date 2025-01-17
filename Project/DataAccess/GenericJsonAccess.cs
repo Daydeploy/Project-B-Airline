5a21072d8048802592c522741b704121b34e09f1
Day <night-deploy@outlook.com>
2025-01-17 03:43:59 +0100

    Exception Handling fix

--- Project/DataAccess/GenericJsonAccess.cs
+++ Project/DataAccess/GenericJsonAccess.cs
@@ -11,14 +11,48 @@ public class GenericJsonAccess<T>
 
     public List<T> LoadAll()
     {
-        var json = File.ReadAllText(_filePath);
-        return JsonSerializer.Deserialize<List<T>>(json);
+        try
+        {
+            var json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<T>>(json);
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"File not found: {_filePath}");
+            Console.WriteLine(ex.Message);
+            return new List<T>();
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine($"Failed to deserialize JSON data, for: {_filePath}");
+            Console.WriteLine(ex.Message);
+            return new List<T>();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error reading file: {_filePath}");
+            Console.WriteLine(e.Message);
+            return new List<T>();
+        }
     }
 
     public void WriteAll(List<T> list)
     {
-        var options = new JsonSerializerOptions { WriteIndented = true };
-        var json = JsonSerializer.Serialize(list, options);
-        File.WriteAllText(_filePath, json);
+        try
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var json = JsonSerializer.Serialize(list, options);
+            File.WriteAllText(_filePath, json);
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine($"Failed to deserialize JSON data for: {_filePath}");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error reading file: {_filePath}");
+            Console.WriteLine(e.Message);
+        }
     }
 }
